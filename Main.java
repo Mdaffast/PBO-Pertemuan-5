a988992d0b4a3c792a128499472a12b62287b71d
Mdaffast <112706163+Mdaffast@users.noreply.github.com>
2022-10-05 22:13:32 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+package LatihanClass3;
+
+public class Main {
+    public static void main(String[] args) {
+        Biodata biodata = new Biodata();
+        biodata.inputData("2021071016", "Muhammad Daffa Stokhorst");
+        biodata.viewData();
+    }
+}
